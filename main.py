cf6dbe80b2489bb92cc124bc7908be97aa1d9f07
keegantm <moseley.k@northeastern.edu>
2024-11-14 16:09:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,51 @@ from PIL import Image
 from streamlit_drawable_canvas import st_canvas
 import pandas as pd
 
+#https://www.youtube.com/watch?v=ADV-AjAXHdc&list=PL2VXyKi-KpYuTAZz__9KVl1jQz74bDG7i&index=4
+def getSkewAngle(cvImage) -> float:
+    # Prep image, copy, convert to gray scale, blur, and threshold
+    newImage = cvImage.copy()
+    blur = cv.GaussianBlur(newImage, (9, 9), 0)
+    thresh = cv.threshold(blur, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
+
+    # Apply dilate to merge text into meaningful lines/paragraphs.
+    # Use larger kernel on X axis to merge characters into single line, cancelling out any spaces.
+    # But use smaller kernel on Y axis to separate between different blocks of text
+    kernel = cv.getStructuringElement(cv.MORPH_RECT, (30, 5))
+    dilate = cv.dilate(thresh, kernel, iterations=2)
+
+    # Find all contours
+    contours, hierarchy = cv.findContours(dilate, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key = cv.contourArea, reverse = True)
+    for c in contours:
+        rect = cv.boundingRect(c)
+        x,y,w,h = rect
+        cv.rectangle(newImage,(x,y),(x+w,y+h),(0,255,0),2)
+
+    # Find largest contour and surround in min area box
+    largestContour = contours[0]
+    print (len(contours))
+    minAreaRect = cv.minAreaRect(largestContour)
+    cv.imwrite("temp/boxes.jpg", newImage)
+    # Determine the angle. Convert it to the value that was originally used to obtain skewed image
+    angle = minAreaRect[-1]
+    if angle < -45:
+        angle = 90 + angle
+    return -1.0 * angle
+# Rotate the image around its center
+def rotateImage(cvImage, angle: float):
+    newImage = cvImage.copy()
+    (h, w) = newImage.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv.getRotationMatrix2D(center, angle, 1.0)
+    newImage = cv.warpAffine(newImage, M, (w, h), flags=cv.INTER_CUBIC, borderMode=cv.BORDER_REPLICATE)
+    return newImage
+
+# Deskew image
+def deskew(cvImage):
+    angle = getSkewAngle(cvImage)
+    return rotateImage(cvImage, -1.0 * angle)
+
 
 def getCameraDataThree():
     camera_result = st.camera_input("Take a level, well lit picture of the table of contents here", key="camera")
@@ -86,13 +131,19 @@ def displayCanvasResults():
     cv.fillPoly(mask, polygon, 255)
 
     #get a copy of the image, but have the pixels outside the polygon all be one color
-    output_image = rgb_img.copy()
-    output_image[mask == 0] = [255, 255, 255]
+    polygon_og_color_mask = rgb_img.copy()
+    polygon_og_color_mask[mask == 0] = [255, 255, 255]
 
-    st.image(output_image)
+    mean_val = cv.mean(polygon_og_color_mask[mask])
+    print("MEAN")
+    print(mean_val)
+    print(rgb_img.shape)
+    st.image(polygon_og_color_mask)
+    
+    mean_val = cv.mean(rgb_img, mask=mask)
 
     
-    st.session_state['mask'] = output_image
+    st.session_state['mask'] = polygon_og_color_mask
     st.session_state['poly'] = polygon
     
 
@@ -151,6 +202,10 @@ with computer_vision_zone:
 
         larger_cropped = cv.resize(grayscale_cropped, (grayscale_cropped.shape[1] * 2, grayscale_cropped.shape[0] * 2))
         st.image(larger_cropped)
+
+        deskewed = deskew(larger_cropped)
+
+        st.image(deskewed)
 
 
 
